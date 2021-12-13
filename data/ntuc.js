ed154b07952d2c5511202e0363c2673ce9bb18fb
ZephyrXxf <jack.see@hotmail.sg>
2021-12-14 03:20:47 +0800

    input geojson

--- data/ntuc.js
+++ data/ntuc.js
@@ -1,4 +1,4 @@
-{
+var ntuc = {
     "type": "FeatureCollection",
     "features": [
       {
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let singapore = [1.3598, 103.8107]; 
-let map = L.map('map').setView(singapore, 13);
+var map = L.map('map').setView(singapore, 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -10,5 +10,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(map);
 
-
-
+L.geoJSON(ntucdata, {
+    function(geoJsonPoint, latlng) {
+        return L.marker(latlng);
+    }
+}).bindPopup(function (layer) {
+    return layer.feature.properties.description;
+}).addTo(map);
